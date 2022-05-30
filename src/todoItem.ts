0a59cba6ceb040996da483a67026253de46e9c09
graphoeil <graphoeil@gmail.com>
2022-05-30 18:40:13 +0200

    refactoring short version

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -1,20 +1,13 @@
 // Class
 class TodoItem{
 
-	// Variables
-	public id:number;
-	public task:string;
-	public complete:boolean = false;
-
 	// Constructor
-	public constructor(id:number, task:string, complete:boolean = false){
-		this.id = id;
-		this.task = task;
-		this.complete = complete;
+	constructor(public id:number, public task:string, public complete:boolean = false){
+		// No statement required
 	};
 
 	// Methods
-	public printDetails(): void{
+	printDetails(): void{
 		console.log(`${ this.id }\t${ this.task } ${ this.complete ? '\t(complete)' : '' }`);
 		
 	};
