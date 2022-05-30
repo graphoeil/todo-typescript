54e1359d9e24650a1e16eb8f532734eb53075377
graphoeil <graphoeil@gmail.com>
2022-05-30 18:43:10 +0200

    adding more comments

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -7,6 +7,7 @@ class TodoItem{
 	};
 
 	// Methods
+	// By default TS assumes that all methods are public unless another access level is used.
 	printDetails(): void{
 		console.log(`${ this.id }\t${ this.task } ${ this.complete ? '\t(complete)' : '' }`);
 		
