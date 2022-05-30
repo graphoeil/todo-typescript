f07945ca8708e97dbcd9b440369f22c85cb447b6
graphoeil <graphoeil@gmail.com>
2022-05-30 19:27:00 +0200

    adding Map and getTodoItems

--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -7,24 +7,40 @@ class TodoCollection{
 
 	// Variables
 	private nextID:number = 1;
+	// A Map allows storing key-value pairs
+	// Here key => id (number) : value (TodoItem)
+	private itemMap = new Map<number, TodoItem>(); // https://howtodoinjava.com/typescript/maps/
 
 	// Constructor
 	constructor(
 		public userName:string,
 		public todoItems:TodoItem[] = []
-	){};
+	){
+		todoItems.forEach((item) => {
+			// As Map is a collection, meaning it has a size, an order
+			// and we can iterate over its keys and values.
+			return this.itemMap.set(item.id, item);
+		});
+	};
 
 	// Methods
 	addTodo(task:string):number{
+		// Get the last nextID and generate another ++
 		while (this.getTodoByID(this.nextID)){
 			this.nextID++;
 		}
-		this.todoItems.push(new TodoItem(this.nextID, task));
+		// map.set(key, value) – adds a new entry in the Map.
+		this.itemMap.set(this.nextID, new TodoItem(this.nextID, task));
 		return this.nextID;
 	};
 	getTodoByID(id:number):TodoItem{
-		return this.todoItems.find((item) => {
-			return item.id === id;
+		// map.get(key) – retrieves the value for a given key from the Map.
+		return this.itemMap.get(id);
+	};
+	getTodoItems(includeComplete:boolean):TodoItem[]{
+		return [ ...this.itemMap.values() ].filter((item) => {
+			// return true (all) or !item.complete ,-)
+			return includeComplete || !item.complete;
 		});
 	};
 	markComplete(id:number, complete:boolean){
