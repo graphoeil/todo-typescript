5e1d6cc9445d35030d238a353e89acf4d8222ed2
graphoeil <graphoeil@gmail.com>
2022-05-30 19:07:12 +0200

    adding some todos in a collection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
+// Imports
+import TodoCollection from "./todoCollection";
+import TodoItem from "./todoItem";
+
+// Variables
+let todos = [
+	new TodoItem(1, 'Buy flowers'),
+	new TodoItem(2, 'Get shoes'),
+	new TodoItem(3, 'Collect tickets'),
+	new TodoItem(4, 'Call Jambon', true)
+];
+let collection = new TodoCollection('Cahouet', todos);
+
+// Show todo list
 console.clear();
-console.log('Cahouet\'s Todo List');
+console.log(`${ collection.userName }'s Todo List`);
+
+// Don't forget that addTodo methods return an id !
+let newID = collection.addTodo('Go for run');
+// getTodoByID return a todoItem object
+let todoItem = collection.getTodoByID(newID);
+// Accessing the printDetails methods of TodoItem object
+todoItem.printDetails();
+
+console.log(JSON.stringify(todoItem));
